a495f3fee356352763bfffc0024503abc6bbdd1f
qhyou11 <yunjianhe@gmail.com>
2021-12-16 16:51:22 +0800

    Create hello.sh

---
+++ hello.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt install python3-pip -y
+sudo pip3 install flask
+mkdir /opt/demo
+cd /opt/demo
+
+
+cat <<EOF > hello.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+EOF
+
+
+export FLASK_APP=hello
+export FLASK_ENV=development
+flask run
